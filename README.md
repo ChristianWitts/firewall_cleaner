d75a24a180c6e21ebe6b607402e229e53b1b09e5
Christian Witts <cwitts@gmail.com>
2023-05-18 12:19:16 +0700

    Initial commit
    Signed-off-by: Christian Witts <cwitts@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# firewall_cleaner
-A tool to help with firewall rule consolidation
+# Firewall Cleaner
+
+A tool to help with firewall rule consolidation, by parsing your HCL containing firewall rules, and determining which could be consolidated based on CIDR ranges and tags.
+
+# Contributing
+
--- src/firewall_cleaner/firewall_cleaner.py
+++ src/firewall_cleaner/firewall_cleaner.py
@@ -4,18 +4,35 @@ Click application to parse firewall rules written for Terraform to look for
 potential rules to deduplicate.
 """
 
+from pprint import PrettyPrinter
+
 import click
 import hcl2
+
+PP = PrettyPrinter(indent=2)
 
 
 @click.group()
 def firewall_cleaner():
-    """Top-level command group
-    """
+    """Top-level command group"""
 
-@firewall_cleaner.command('gcp')
-@click.option('--source', help='File containing compure firewall rules')
+
+@firewall_cleaner.command("gcp")
+@click.option("--source", help="File containing compure firewall rules")
 def gcp(source: str):
-    """
-    """
-    pass
+    """ """
+    with open(source, "r") as fin:
+        data = hcl2.load(fin)
+
+    for fw in data["resource"]:
+        tags = get_tags(get_inner_dict(fw["google_compute_firewall"]))
+        PP.pprint(tags)
+
+
+def get_tags(d):
+    return d.get("target_tags", [[]])[0]
+
+
+def get_inner_dict(d):
+    k = list(d.keys())[0]
+    return d[k]
